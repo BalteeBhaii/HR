2ac6d2ea90773f8fba7a70168cadf67cd8a4da10
balteebhaii <webd54402@gmail.com>
2024-03-19 21:36:56 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Controllers\Backend\DashboardController;
+
+Route::get('/' , [AuthController::class , 'index']);
+Route::get('forgot-password', [AuthController::class, 'forgot_password']);
+Route::get('register', [AuthController::class, 'register']);
+Route::post('register_post' , [AuthController::class, 'register_post']);
+Route::post('checkemail' , [AuthController::class, 'CheckEmail']);
+Route::post('login_post' , [AuthController::class, 'login_post']);
+
+// Admin HR
+Route::middleware([AdminMiddleware::class])->group(function(){
+
+    Route::get('admin/dashboard', [DashboardController::class , 'dashboard']);
+
+});
+
+Route::get('logout', [AuthController::class, 'logout']);
